2767cc5d9a04e40912c9ac06a70e0024f1a55bfd
Masedie <masedie@Masedies-MacBook-Air.local>
2024-09-06 14:17:13 +0200

    0. Basic application

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import holbertonLogo from './holberton-logo.jpg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={holbertonLogo} className="App-logo" alt="Holberton Logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+      </body>
+      <footer className="App-footer">
+        <p>Copyright 2020 - Holberton School</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
